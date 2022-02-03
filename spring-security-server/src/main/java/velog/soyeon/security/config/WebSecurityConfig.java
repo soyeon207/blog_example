07c2b4c5d4580a3857593965f3a33094122fcda6
soyeon207 <psy010207@naver.com>
2022-02-03 22:57:07 +0900

    #4 [DELETED] 필요 없는 WebMvcConfig 삭제

--- spring-security-server/src/main/java/velog/soyeon/security/config/WebSecurityConfig.java
+++ spring-security-server/src/main/java/velog/soyeon/security/config/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package velog.soyeon.security.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -24,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 정적 
     public void configure(WebSecurity web) {
         web
                 .ignoring() // spring security 필터 타지 않도록 무시
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()) // 정적 자원 무시
+                .antMatchers("/resources/**")
                 .antMatchers("/h2-console/**"); // h2-console 무시
     }
 
