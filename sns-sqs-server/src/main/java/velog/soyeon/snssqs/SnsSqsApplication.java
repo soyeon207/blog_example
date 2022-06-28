cd3cc0923e8d0b39fa660c1948323e6402c63be8
soyeon207 <psy010207@naver.com>
2022-06-29 02:52:11 +0900

    Feat: sns-sqs 모듈 추가 (#5)

---
+++ sns-sqs-server/src/main/java/velog/soyeon/snssqs/SnsSqsApplication.java
@@ -0,0 +1,11 @@
+package velog.soyeon.snssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsSqsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnsSqsApplication.class, args);
+    }
+}
